a2afb8d13bbc868774312c687f8cfae61cd2b56b
chobi999 <kunio@netagency.biz>
2015-03-10 10:25:16 +0000

    Add user following

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,21 @@ class UsersController < ApplicationController
   def edit
   end
 
+
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.followed_users.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+
   def update
     if @user.update_attributes(user_params)
       flash[:success] = "Profile updated"
